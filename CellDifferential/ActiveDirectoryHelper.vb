79362cf5369bbd4d2d421fc2410234ec57bbfe5a
balee323 <brienzhere@yahoo.com>
2019-11-07 21:58:44 +0900

    progress checkin

---
+++ CellDifferential/ActiveDirectoryHelper.vb
@@ -0,0 +1,48 @@
+﻿Imports System.DirectoryServices
+
+
+Public Structure UserInfo
+    Dim UserName As String
+    Dim GivenName As String
+End Structure
+
+Public Module ActiveDirectoryHelper
+
+    Public Function GetUserInfo() As UserInfo
+
+        Dim entry As New DirectoryEntry()
+
+        Dim userName As String = Environment.UserName.ToString
+
+        entry.Username = userName
+
+        Dim dirSearcher = New DirectorySearcher(entry)
+
+        dirSearcher.Filter = "(SAMAccountName=" & userName & ")"
+
+        dirSearcher.PropertiesToLoad.Add("GivenName")
+        dirSearcher.PropertiesToLoad.Add("sn")
+
+        Dim sr As SearchResult
+
+        Try
+            sr = dirSearcher.FindOne()
+        Catch ex As Exception
+            'user name not found in active directory
+        End Try
+
+
+        Dim result As String = String.Empty
+
+        If sr Is Nothing Then
+            result = userName
+        Else
+            result = sr.GetDirectoryEntry.Properties("sn").Value.ToString() + ", " + sr.GetDirectoryEntry.Properties("GivenName").Value.ToString()
+        End If
+
+        Return New UserInfo()
+
+    End Function
+
+
+End Module
--- CellDifferential/SqlLiteManager.vb
+++ CellDifferential/SqlLiteManager.vb
@@ -1,6 +1,10 @@
 ﻿
 Imports System.Data.SQLite
 
+
+
+
+
 Public Class SqlLiteManager
     Dim configDb As String = "WBCDiffSettings.db"
     Dim connectionString As String = "Data Source={0};Version=3;"
@@ -26,7 +30,7 @@ Public Class SqlLiteManager
         End If
 
         CreateUserSettingsTable()
-        insert_config_data()
+        InsertUser()
         ViewData()
 
 
@@ -37,7 +41,7 @@ Public Class SqlLiteManager
         Dim create_table As String = String.Empty
 
         ' create table sql statement
-        create_table &= "CREATE TABLE MyTable(UserID INTEGER PRIMARY KEY ASC, FirstName VARCHAR(25));"
+        create_table &= "CREATE TABLE MyTable(UserID INTEGER PRIMARY KEY ASC, FirstName VARCHAR(25), LastName VARCHAR(25));"
 
 
         Try
@@ -55,17 +59,19 @@ Public Class SqlLiteManager
 
     End Sub
 
-    Public Sub insert_config_data()
+    Public Sub InsertUser()
         Dim dt As New DataTable
         Dim SQL As String = String.Empty
 
-        ' create datatable
-        dt.Columns.Add(New DataColumn("CustomerID"))
-        dt.Columns.Add(New DataColumn("FirstName"))
 
-        ' add default setting records
-        dt.Rows.Add({"1234", "Brian"})
-        dt.Rows.Add({"444444", "Connor"})
+
+        Dim userName As String = String.Empty
+
+        Try
+            userName = Environment.UserName.ToString()
+        Catch ex As Exception
+
+        End Try
 
 
         Try
@@ -78,33 +84,77 @@ Public Class SqlLiteManager
                 Using transaction
                     Using cmd As New SQLiteCommand(con)
                         cmd.Transaction = transaction
-                        For Each row As DataRow In dt.Rows
-                            ' create the SQL statement
-                            SQL = ""
-                            SQL &= "INSERT INTO MyTable (CustomerId, FirstName) VALUES "
-                            SQL &= String.Format("('{0}','{1}')", row("CustomerId"), row("FirstName"))
 
-                            cmd.CommandText = SQL
-                            cmd.ExecuteNonQuery()
+                        ' create the SQL statement
+                        SQL &= "INSERT INTO UserInfo (UserName, DateCreated) VALUES "
+                        SQL &= $"{userName}, {Date.Now.ToString()}"
 
-                        Next
+                        cmd.CommandText = SQL
+                        cmd.ExecuteNonQuery()
+
                     End Using
                     transaction.Commit()
                 End Using
             End Using
-            MessageBox.Show("Insert Default Setting Success")
+            MessageBox.Show("Insert User Success")
 
         Catch ex As Exception
-            MessageBox.Show("Insert Default Setting Failed")
+            MessageBox.Show("Insert User Failed")
         End Try
 
     End Sub
+
+
+    'Public Sub insert_config_data()
+    '    Dim dt As New DataTable
+    '    Dim SQL As String = String.Empty
+
+    '    ' create datatable
+    '    dt.Columns.Add(New DataColumn("UserID"))
+    '    dt.Columns.Add(New DataColumn("FirstName"))
+
+    '    ' add default setting records
+    '    dt.Rows.Add({"1234", "Brian"})
+    '    dt.Rows.Add({"444444", "Connor"})
+
+
+    '    Try
+
+    '        'insert the records into the database table
+
+    '        Using con As New SQLiteConnection(connectionString)
+    '            con.Open()
+    '            Dim transaction As SQLiteTransaction = con.BeginTransaction()
+    '            Using transaction
+    '                Using cmd As New SQLiteCommand(con)
+    '                    cmd.Transaction = transaction
+    '                    For Each row As DataRow In dt.Rows
+    '                        ' create the SQL statement
+    '                        SQL = ""
+    '                        SQL &= "INSERT INTO MyTable (CustomerId, FirstName) VALUES "
+    '                        SQL &= String.Format("('{0}','{1}')", row("CustomerId"), row("FirstName"))
+
+    '                        cmd.CommandText = SQL
+    '                        cmd.ExecuteNonQuery()
+
+    '                    Next
+    '                End Using
+    '                transaction.Commit()
+    '            End Using
+    '        End Using
+    '        MessageBox.Show("Insert Default Setting Success")
+
+    '    Catch ex As Exception
+    '        MessageBox.Show("Insert Default Setting Failed")
+    '    End Try
+
+    'End Sub
 
     Public Sub ViewData()
 
 
         ' create table sql statement
-        Dim queryStr = "Select * from mytable"
+        Dim queryStr = "Select * from UserInfo"
 
 
         Try
