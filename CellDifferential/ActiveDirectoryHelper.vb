017fa9140c486a51e2a4b58e8ae308fafd760511
balee323 <brienzhere@yahoo.com>
2019-11-19 16:37:16 +0900

    commented out active directory code as it was locking up the program.

--- CellDifferential/ActiveDirectoryHelper.vb
+++ CellDifferential/ActiveDirectoryHelper.vb
@@ -9,26 +9,26 @@ Public Module ActiveDirectoryHelper
         Dim userName As String = Environment.UserName.ToString
         entry.Username = userName
 
-        Dim dirSearcher = New DirectorySearcher(entry)
-        dirSearcher.Filter = "(SAMAccountName=" & userName & ")"
-        dirSearcher.PropertiesToLoad.Add("GivenName")
-        dirSearcher.PropertiesToLoad.Add("sn")
+        'Dim dirSearcher = New DirectorySearcher(entry)
+        'dirSearcher.Filter = "(SAMAccountName=" & userName & ")"
+        'dirSearcher.PropertiesToLoad.Add("GivenName")
+        'dirSearcher.PropertiesToLoad.Add("sn")
 
-        Dim sr As SearchResult = Nothing
+        'Dim sr As SearchResult = Nothing
 
-        Try
-            sr = dirSearcher.FindOne()
-        Catch ex As Exception
-            'user name not found in active directory
-        End Try
+        'Try
+        '    sr = dirSearcher.FindOne()
+        'Catch ex As Exception
+        '    'user name not found in active directory
+        'End Try
 
         Dim userSettings As New UserInfo()
         userSettings.UserName = userName
         userSettings.GivenName = "Not Found"
 
-        If sr IsNot Nothing Then
-            userSettings.GivenName = sr.GetDirectoryEntry.Properties("GivenName").Value.ToString()
-        End If
+        'If sr IsNot Nothing Then
+        '    userSettings.GivenName = sr.GetDirectoryEntry.Properties("GivenName").Value.ToString()
+        'End If
 
         Return userSettings
 
